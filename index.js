975f9d26bd1acd087020a8bdd2e8a416ed5aa278
심규장 Gyu Jang Sim <gyujang95@gmail.com>
2020-08-17 17:23:11 +0900

    gitignore

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListen() {
+  console.log(`Listening on http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("hello~");
+}
+
+function handleProfile(req, res) {
+  res.send("you are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListen);
