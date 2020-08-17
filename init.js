5bf7f681353aa87fb2564c9667e4e5f81b868662
심규장 Gyu Jang Sim <gyujang95@gmail.com>
2020-08-18 02:24:17 +0900

    routers

--- init.js
+++ init.js
@@ -3,6 +3,6 @@ import app from "./app";
 const PORT = 4000;
 
 const hadleListening = () =>
-  console.log(`✅Listening on: https://localhost${PORT}`);
+  console.log(`✅ Listening on: https://localhost${PORT}`);
 
 app.listen(PORT, hadleListening);
